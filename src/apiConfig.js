e42dc1440a1ed81d0e62be75699330059757b62a
salman778 <alk-333@hotmail.com>
2019-07-30 16:21:46 +0300

    for deploy

---
+++ src/apiConfig.js
@@ -0,0 +1,13 @@
+let apiUrl
+const apiUrls = {
+  production: 'https://thawing-eyrie-30806.herokuapp.com',
+  development: 'http://localhost:8000'
+}
+
+if (window.location.hostname === 'localhost') {
+  apiUrl = apiUrls.development
+} else {
+  apiUrl = apiUrls.production
+}
+
+export default apiUrl
